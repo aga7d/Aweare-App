25c776f609bb088680ccc43cf92b91ac15ee09fe
Aga <agidabrow@gmail.com>
2019-06-08 22:20:19 +0200

    final

--- client/src/components/NextButton.js
+++ client/src/components/NextButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Link} from "react-router-dom"
 const NextButton =(props)=>{
     return  (
-        <button onClick={()=>{console.log("siemka")}} className= "btn">
+        <button className= "btn">
          <Link to= {props.link}>
          <i className="fas fa-long-arrow-alt-right"></i>
          </Link>
